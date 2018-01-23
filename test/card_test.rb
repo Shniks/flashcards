8d8242f71b1c5b426b398d4d96844c98a2a4dea3
Lihkin <shniks@gmail.com>
2018-01-23 05:25:36 -0700

    Creating card test file. Completing Iteration 1.

---
+++ test/card_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+
+class CardTest < Minitest::Test
+
+  def test_if_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau")
+
+    assert_instance_of Card, card
+  end
+
+  def test_if_it_has_a_question
+    card = Card.new("What is the capital of Alaska?", "Juneau")
+
+    assert_equal "What is the capital of Alaska?", card.question
+  end
+
+  def test_if_it_has_an_answer
+    card = Card.new("What is the capital of Alaska?", "Juneau")
+
+    assert_equal "Juneau", card.answer
+  end
+  
+end
