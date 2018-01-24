3c218570ec7e9ddaf1a51548a045c4e9c5465e39
Lihkin <shniks@gmail.com>
2018-01-23 20:50:12 -0700

    Refactoring card_generator methods to accept a hint from the Card class.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -17,13 +17,13 @@ class CardGenerator
   def number_of_cards
     contents = File.read(filename)
     @data = contents.split(/[\n,]/)
-    @total_cards = @data.length / 2
+    @total_cards = @data.length / 3
   end
 
   def cards
     number_of_cards.times do
-      all_cards << Card.new(@data[0], @data[1])
-      @data.slice!(0..1)
+      all_cards << Card.new(@data[0], @data[1], @data[2])
+      @data.slice!(0..2)
     end
     all_cards
   end
