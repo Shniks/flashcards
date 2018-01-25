97e1faa21feb032cb0c9d24d75f5b391b782e7e7
Lihkin <shniks@gmail.com>
2018-01-24 22:06:07 -0700

    Refactoring runner file to call all methods in the game play file sequentially.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,7 +3,14 @@ require './lib/game_play.rb'
 class FlashCardRunner
 
   def self.start_game
-    GamePlay.new.game_response
+    game = GamePlay.new
+    game.get_user_name
+    game.get_user_deck_input
+    game.assign_correct_file_based_on_user_deck_choice
+    game.initialize_game
+    game.play_game
+    game.game_response
+    game.game_result
   end
 
 end
