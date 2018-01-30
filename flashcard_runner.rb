7f8d97d2391e8624c9d50064cdaeccca08349563
Lihkin <shniks@gmail.com>
2018-01-29 19:09:43 -0700

    Refactoring runner file to remove assign deck method as it's being called in the GamePlay class in another method.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,7 +3,6 @@ require './lib/game_play.rb'
     game = GamePlay.new
     game.get_user_name
     game.get_user_deck_input
-    game.assign_deck
     game.initialize_game
     game.play_game
     game.game_response
